5455d05f265a924c700666849c8271dd334630fe
cretu <cretu@localhost>
2011-06-14 02:19:41 +0300

    Added AJAX interaction to the lecture session pages and more client-side functionality for the same section

--- apps/lectures/admin.py
+++ apps/lectures/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from lectures.models import Lecture
+from lectures.models import Lecture, NotesUpdate
 
 admin.site.register(Lecture)
+admin.site.register(NotesUpdate)
